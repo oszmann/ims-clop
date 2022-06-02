4c77e69fbf2c94e8fe4d99b9a8a1749235d5078d
Fabian Vizi <f.vizi@tu-braunschweig.de>
2022-06-02 21:13:54 +0200

    feat: add helper class for item/ helper enum for routes and deleting functionality

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,19 +1,64 @@
 import {a} from "../common/util";
+import { Item } from "./item";
 export {}
 
-
-console.log("hi");
-console.log(a);
-async function b() {
-    console.log("hi :)");
-}
-b();
 document.getElementById("1234").innerHTML = a;
 
-const button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
-button.addEventListener("click", async () => {
-    const c = await fetch("http://localhost:3000/api").then(resp => {
-        
-        console.log(resp.json());
+//HTML CONSTS
+const updateButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+const itemsDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("items");
+const deleteButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("delete");
+
+//API ROUTES
+enum Routes {
+    C = "http://localhost:3000/api/set",
+    R = "http://localhost:3000/api/get",
+    U = "http://localhost:3000/api/set",
+    D = "http://localhost:3000/api/remove"
+}
+updateButton.addEventListener("click", async () => {
+    makeRequest(Routes.R)
+        .then(resp => {
+        console.log(resp);
     });
 });
+deleteButton.addEventListener("click", () => {
+    makeRequest(Routes.D, "/all")
+    .then((res) => {
+        console.log(res);
+    });
+})
+
+let doUpdate: boolean = true; 
+let items: Item[] = [];
+
+async function makeRequest(type: Routes, request: string = ""): Promise<Item[]>{
+    return <Item[]> await (await fetch(type + request)).json();
+}
+
+function createItemDiv(item: Item): HTMLDivElement {
+    const div: HTMLDivElement = document.createElement("div");
+    div.setAttribute("id", item.id);
+    div.classList.add("item-outer-div");
+    return div
+}
+
+async function main() {
+
+    const items =  await makeRequest(Routes.R);
+    console.log(items)
+
+    items.forEach(item => {
+
+    });
+
+
+
+    async function update() {
+        doUpdate = false;
+    }
+    if (doUpdate) {
+        update()
+    }
+}
+main();
