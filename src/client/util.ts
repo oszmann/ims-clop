f77eda51c9de0a7934e7ce531874dc00ddc2c45b
Fabian Vizi <fabian.vizi1@gmail.com>
2022-06-13 14:03:24 +0200

    fix: remove unwanted html elements import new ones to util.ts

--- src/client/util.ts
+++ src/client/util.ts
@@ -6,6 +6,8 @@ export enum Route {
     D = "http://localhost:3000/api/remove",
 }
 
+export const localhost = "http://localhost:3000";
+
 export enum VarType {
     item = "?item=",
     location = "&loc=",
@@ -19,17 +21,25 @@ export enum SortBy {
     DESCRIPTION,
 }
 
-//STATIC
-export const addItemDiv = <HTMLDivElement>$("insert-div");
-export const add0 = <HTMLTextAreaElement>$("insert-0");
-export const add1 = <HTMLTextAreaElement>$("insert-1");
-export const add2 = <HTMLTextAreaElement>$("insert-2");
-export const add3 = <HTMLTextAreaElement>$("insert-3");
-export const addButton = <HTMLButtonElement>$("add-button");
-export const openAddItem = <HTMLButtonElement>$("button");
+//STATIC - INDEX
+export const openAddItem = <HTMLButtonElement>$("insert-item");
 export const deleteButton = <HTMLButtonElement>$("delete");
 export const openAddLocation = <HTMLButtonElement>$("insert-location");
-export const itemsDiv = <HTMLDivElement>$("items");
+
+//STATIC - LOCATIONS
+export const addWarehouse = <HTMLTextAreaElement>$("insert-warehouse");
+export const addRow = <HTMLTextAreaElement>$("insert-row");
+export const addRack = <HTMLTextAreaElement>$("insert-rack");
+export const addShelf = <HTMLTextAreaElement>$("insert-shelf");
+export const addLocationButton = <HTMLButtonElement>$("add-location-button");
+export const homeButton = <HTMLButtonElement>$("home");
+
+//STATIC - ITEMS
+export const addPartNo = <HTMLTextAreaElement>$("insert-part-no");
+export const addDescription = <HTMLTextAreaElement>$("insert-description");
+export const addCost = <HTMLTextAreaElement>$("insert-cost");
+export const addItemButton = <HTMLButtonElement>$("add-item-button");
+export const itemsDiv = <HTMLDivElement>$("items-div");
 
 export function $<T extends HTMLElement>(id: string): T {
     return <T>document.getElementById(id);
